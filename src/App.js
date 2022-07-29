d823bd865e0624e00b05a034ca8453b6b0d0fc2d
apmfree78 <amit@profitswami.com>
2022-07-29 12:24:41 -0400

    application complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [active, setActive] = useState([false, false, false, false, false]);
+
+  const activate = (pos) => {
+    const _active = [...active];
+
+    for (let i = 0; i < _active.length; i++) {
+      if (i <= pos) _active[i] = true;
+      else _active[i] = false;
+    }
+
+    // set state
+    setActive([..._active]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ratings'>
+      {active.map((isActive, i) => {
+        if (isActive)
+          return (
+            <>
+              <span onClick={() => activate(i)} className='active'>
+                <h2>Active</h2>
+              </span>
+              <br />
+            </>
+          );
+        else
+          return (
+            <>
+              <span onClick={() => activate(i)}>
+                <h2>InActive</h2>
+              </span>
+              <br />
+            </>
+          );
+      })}
     </div>
   );
 }
